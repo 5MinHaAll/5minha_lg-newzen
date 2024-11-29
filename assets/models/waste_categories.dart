58b44de44414643f819d0f821b821c34d32359b3
yuna <yhpark22@dgu.ac.kr>
2024-11-29 12:02:13 +0900

    Init: 음식물 쓸기 분류 가이드 추가

---
+++ assets/models/waste_categories.dart
@@ -0,0 +1,35 @@
+const wasteCategories = {
+  "processable": [
+    {"name": "Apple", "name_ko": "사과", "status": "처리 가능"},
+    {"name": "Banana", "name_ko": "바나나", "status": "처리 가능"},
+    {"name": "Rice", "name_ko": "밥", "status": "처리 가능"},
+    {"name": "Tomato", "name_ko": "토마토", "status": "처리 가능"},
+    {"name": "Cucumber", "name_ko": "오이", "status": "처리 가능"},
+    {"name": "Fish", "name_ko": "생선", "status": "처리 가능"},
+    {"name": "Meat", "name_ko": "고기", "status": "처리 가능"},
+    {"name": "Bread", "name_ko": "빵", "status": "처리 가능"},
+    {"name": "Pancake", "name_ko": "팬케이크", "status": "처리 가능"},
+    {"name": "Tofu", "name_ko": "두부", "status": "처리 가능"},
+  ],
+  "caution": [
+    {"name": "Banana-peel", "name_ko": "바나나 껍질", "status": "주의 필요"},
+    {"name": "Apple-core", "name_ko": "사과 심", "status": "주의 필요"},
+    {"name": "Drink", "name_ko": "음료", "status": "주의 필요"},
+    {"name": "Potato", "name_ko": "감자", "status": "주의 필요"},
+    {
+      "name": "High-fiber Vegetables",
+      "name_ko": "섬유질 많은 채소",
+      "status": "주의 필요"
+    },
+    {"name": "Orange-peel", "name_ko": "오렌지 껍질", "status": "주의 필요"},
+    {"name": "Shrimp", "name_ko": "새우", "status": "주의 필요"},
+  ],
+  "nonProcessable": [
+    {"name": "Bone", "name_ko": "뼈", "status": "처리 불가"},
+    {"name": "Bone-fish", "name_ko": "생선 뼈", "status": "처리 불가"},
+    {"name": "Mussel-shell", "name_ko": "조개껍데기", "status": "처리 불가"},
+    {"name": "Egg-shell", "name_ko": "달걀 껍질", "status": "처리 불가"},
+    {"name": "Shrimp-shell", "name_ko": "새우 껍질", "status": "처리 불가"},
+    {"name": "Other-waste", "name_ko": "기타 쓰레기", "status": "처리 불가"},
+  ],
+};
